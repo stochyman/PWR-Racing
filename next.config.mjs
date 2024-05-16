9dbc6548a3666036a9dd05c49a3cf420bf03baaf
Dawid Chmal <d.chmalu@gmail.com>
2024-05-16 13:15:57 +0200

    Remove appDir from next.config.mjs and fix dynamic routing issues

--- next.config.mjs
+++ next.config.mjs
@@ -8,31 +8,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/team",
+        destination: "/team/RT14e",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'storage.googleapis.com',
-//         pathname: '/pwr-rt/*',
-//       },
-//     ],
-//   },
-//   async redirects() {
-//     return [
-//       // Basic redirect
-//       {
-//         source: '/team',
-//         destination: '/team/RT13e',
-//         permanent: true,
-//       },
-//     ]
-//   },
-// };
-
-// export default nextConfig;
