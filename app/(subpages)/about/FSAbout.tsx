98da70caedcc338eb4b8de1a9366b2114fbc3729
Dawid Chmal <d.chmalu@gmail.com>
2024-06-03 21:27:53 +0200

    zdjecia

--- app/(subpages)/about/FSAbout.tsx
+++ app/(subpages)/about/FSAbout.tsx
@@ -6,79 +6,81 @@ import Heading from "@/app/components/Heading";
 
 const FSAbout: React.FC = () => {
   return (
-    <Container>
-      <div className="w-full">
-        <div className="">
-          <Title wrap size="medium" color="black">
-            CZTERY KATEGORIE DYNAMICZNE
-          </Title>
-          <div className=" grid grid-cols-1 sm:grid-cols-2  md:grid-cols-4 gap-6 my-6">
-            <FlipCard
-              height={"h-60"}
-              src="/images/FS/acceleration.png"
-              title="Acceleration"
-              text="Konkurencja, która polega na jak najszybszym
+    <div className=" relative bg-white">
+      <Container>
+        <div className="w-full">
+          <div className="">
+            <Title wrap size="medium" color="black">
+              CZTERY KATEGORIE DYNAMICZNE
+            </Title>
+            <div className=" grid grid-cols-1 sm:grid-cols-2  md:grid-cols-4 gap-6 my-6">
+              <FlipCard
+                height={"h-60"}
+                src="/images/FS/acceleration.png"
+                title="Acceleration"
+                text="Konkurencja, która polega na jak najszybszym
                 pokonaniu 75 metrów, dzięki czemu możliwe jest ocenienie
                 przyspieszenia bolidu. Zespoły mają do dyspozycji dwie próby i
                 lepsza z nich jest oceniana."
-            />
-            <FlipCard
-              height={"h-60"}
-              src="/images/FS/skipad.png"
-              title="Skid Pad"
-              text="Konkurencja polegająca na jeździe po “ósemce”.
+              />
+              <FlipCard
+                height={"h-60"}
+                src="/images/FS/skipad.png"
+                title="Skid Pad"
+                text="Konkurencja polegająca na jeździe po “ósemce”.
                 Każdy Zespół ma do dyspozycji dwie próby, z których przy
                 punktowaniu brana jest pod uwagę lepsza z nich."
-            />
-            <FlipCard
-              height={"h-60"}
-              src="/images/FS/autocross.png"
-              title="Autocross"
-              text="Konkurencja, podczas której uczestnicy mają za
+              />
+              <FlipCard
+                height={"h-60"}
+                src="/images/FS/autocross.png"
+                title="Autocross"
+                text="Konkurencja, podczas której uczestnicy mają za
                 zadanie pokonać jedno okrążenie na torze w jak najkrótszym
                 czasie. W konkurencji może wziąć udział dwóch kierowców z danego
                 Zespołu, a każdy z nich ma do dyspozycji dwa przejazdy. Pod
                 uwagę brany jest najlepszy czas z czterech."
-            />
-            <FlipCard
-              height={"h-60"}
-              src="/images/FS/endurance.png"
-              title="Endurance"
-              text="Konkurencja polegająca na wyścigu na około 22
+              />
+              <FlipCard
+                height={"h-60"}
+                src="/images/FS/endurance.png"
+                title="Endurance"
+                text="Konkurencja polegająca na wyścigu na około 22
                 kilometry. Po przejechaniu 11 kilometrów następuje zmiana
                 kierowcy, na którą Zespół ma 3 minuty. Jest to najwyżej
                 punktowana konkurencja, ale punkty otrzymują tylko Zespoły,
                 które ukończyły przejazd."
-            />
+              />
+            </div>
+          </div>
+          <div className=" my-8 md:my-12">
+            <Title wrap size="medium" color="black">
+              TRZY KATEGORIE STATYCZNE
+            </Title>
+            <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 my-6">
+              <FlipCard
+                height={"h-80"}
+                src="/images/FS/cost.png"
+                title="Cost and Manufacturing"
+                text="Konkurencja oceniają skrupulatność, z jaką przygotowano kosztorys projektu oraz wiedzę Członków Zespołu na temat kosztów materiałów oraz wytworzenia bolidu."
+              />
+              <FlipCard
+                height={"h-80"}
+                src="/images/FS/engineering.png"
+                title="Engineering Design"
+                text="Konkurencja polegająca na prezentacji projektu bolidu. Sędziowie oceniają umiejętności inżynierskie oraz pomysłowość Członków Zespołów przy projektowaniu zarówno całokształtu, jak i poszczególnych systemów pojazdu. Jest to najwyżej punktowana konkurencja statyczna, którą oceniają eksperci z branży motoryzacyjnej."
+              />
+              <FlipCard
+                height={"h-80"}
+                src="/images/FS/business.png"
+                title="Business Plan Presentation"
+                text="Konkurencja, podczas której Członkowie Zespołu wcielają się w rolę zarządu start-upu, przykładowo tworzonych na zamówienie hamulców. Za pomocą dziesięciominutowej prezentacji mają za zadanie przekonać sędziów odgrywających inwestorów do wsparcia ich modelu biznesowego."
+              />
+            </div>
           </div>
         </div>
-        <div className=" my-8 md:my-12">
-          <Title wrap size="medium" color="black">
-            TRZY KATEGORIE STATYCZNE
-          </Title>
-          <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 my-6">
-            <FlipCard
-              height={"h-80"}
-              src="/images/FS/cost.png"
-              title="Cost and Manufacturing"
-              text="Konkurencja oceniają skrupulatność, z jaką przygotowano kosztorys projektu oraz wiedzę Członków Zespołu na temat kosztów materiałów oraz wytworzenia bolidu."
-            />
-            <FlipCard
-              height={"h-80"}
-              src="/images/FS/engineering.png"
-              title="Engineering Design"
-              text="Konkurencja polegająca na prezentacji projektu bolidu. Sędziowie oceniają umiejętności inżynierskie oraz pomysłowość Członków Zespołów przy projektowaniu zarówno całokształtu, jak i poszczególnych systemów pojazdu. Jest to najwyżej punktowana konkurencja statyczna, którą oceniają eksperci z branży motoryzacyjnej."
-            />
-            <FlipCard
-              height={"h-80"}
-              src="/images/FS/business.png"
-              title="Business Plan Presentation"
-              text="Konkurencja, podczas której Członkowie Zespołu wcielają się w rolę zarządu start-upu, przykładowo tworzonych na zamówienie hamulców. Za pomocą dziesięciominutowej prezentacji mają za zadanie przekonać sędziów odgrywających inwestorów do wsparcia ich modelu biznesowego."
-            />
-          </div>
-        </div>
-      </div>
-    </Container>
+      </Container>
+    </div>
   );
 };
 
@@ -93,12 +95,12 @@ interface FlipCardProps {
 
 const FlipCard: React.FC<FlipCardProps> = ({ text, src, title, height }) => {
   return (
-    <div className={`flip-card ${height} relative rounded-md overflow-hidden`}>
+    <div className={`flip-card ${height} relative rounded-md`}>
       <div className="flip-card-inner">
         <div className="flip-card-front relative w-full h-full">
           <Image src={src} alt={title} layout="fill" objectFit="cover" />
         </div>
-        <div className="flip-card-back absolute inset-0 z-20 flex flex-col justify-center items-center gap-4 p-8 bg-neutral-200">
+        <div className="flip-card-back absolute inset-0 z-20 flex flex-col justify-center items-center gap-4 rounded-md p-8 bg-neutral-200">
           <Title color="black" size="small">
             {title}
           </Title>
--- app/components/Section/FSSection/FSSection.tsx
+++ app/components/Section/FSSection/FSSection.tsx
@@ -13,10 +13,13 @@ interface FSSectionProps {
 
 const FSSection: React.FC<FSSectionProps> = ({ aboutUs }) => {
   return (
-    <div id="formulastudent-section" className="py-8 md:py-20">
+    <div
+      id="formulastudent-section"
+      className=" relative py-8 md:py-20 bg-white"
+    >
       <Container>
         <div className="">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-20 ">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-20">
             <div className="">
               {aboutUs && (
                 <div className="my-6">
